fc71acb8c64da2b99a0b797d9fc7325b3af8dafc
Carlos Granados <carlos@i6.io>
2019-12-08 10:54:28 +0000

    Update Excel5.php

--- Classes/PHPExcel/Reader/Excel5.php
+++ Classes/PHPExcel/Reader/Excel5.php
@@ -1059,6 +1059,7 @@ class PHPExcel_Reader_Excel5 extends PHPExcel_Reader_Abstract implements PHPExce
                             // picture
                             // get index to BSE entry (1-based)
                             $BSEindex = $spContainer->getOPT(0x0104);
+                            if (empty($BSEindex)) break;
                             $BSECollection = $escherWorkbook->getDggContainer()->getBstoreContainer()->getBSECollection();
                             $BSE = $BSECollection[$BSEindex - 1];
                             $blipType = $BSE->getBlipType();
